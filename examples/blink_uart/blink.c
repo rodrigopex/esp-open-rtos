d99abba753fbd3d5953bffbdacf3f18981fba386
rodrigopex <rodrigopex@gmail.com>
2016-08-19 15:23:50 -0300

    Periodic task added to the example

--- examples/blink_uart/blink.c
+++ examples/blink_uart/blink.c
@@ -23,13 +23,25 @@
 //#define WIFI_SSID "GVT-C3C5"
 //#define WIFI_PASS "5403000556"
 
-void blinkenTask(void *pvParameters)
+void blinkTicTask(void *pvParameters)
 {
+    portTickType lastTick = xTaskGetTickCount();
+    const uint32_t period = 2000 / portTICK_RATE_MS;
     while(1) {
-        printf("tic %.2f\n", 1.3 + 3.5);
-        vTaskDelay(1000 / portTICK_RATE_MS);        
-        printf("tac\n");
-        vTaskDelay(1000 / portTICK_RATE_MS);
+        vTaskDelayUntil(&lastTick, period);
+        printf("+ tic %u, %u\n", lastTick, xTaskGetTickCount());
+    }
+    vTaskDelete(NULL);
+}
+
+void blinkTacTask(void *pvParameters)
+{
+    portTickType lastTick = xTaskGetTickCount();
+    const uint32_t period = 2000 / portTICK_RATE_MS;
+    vTaskDelay(100);
+    while(1) {
+        vTaskDelayUntil(&lastTick, period);
+        printf("- tac %u, %u\n", lastTick, xTaskGetTickCount());
     }
     vTaskDelete(NULL);
 }
@@ -37,5 +49,8 @@ void blinkenTask(void *pvParameters)
 void user_init(void)
 {
     uart_set_baud(UART0, 115200);
-    xTaskCreate(blinkenTask, (signed char *)"blinkenTask", 256, NULL, 2, NULL);    
+    vTaskDelay(300);
+    xTaskCreate(blinkTicTask, (signed char *)"blinkTicTask", 256, NULL, 3, NULL);
+    vTaskDelay(100);
+    xTaskCreate(blinkTacTask, (signed char *)"blinkTacTask", 256, NULL, 3, NULL);
 }
